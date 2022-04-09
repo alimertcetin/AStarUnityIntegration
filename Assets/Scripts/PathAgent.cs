9b0dd8dccb02fc52c094cf5c9e6f243b673cda0c
alimertcetin <mrtmert54@gmail.com>
2022-04-09 21:21:59 +0300

    Added obstacles and couple of agent

--- Assets/Scripts/PathAgent.cs
+++ Assets/Scripts/PathAgent.cs
@@ -2,6 +2,7 @@ using AStarPathFinding;
 using AStarPathFinding.UnityIntegration;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PathAgent : MonoBehaviour
@@ -26,7 +27,7 @@ public class PathAgent : MonoBehaviour
     {
         //Plane scale must be multiplied by 10
         PathGrid pathGrid = new PathGrid(ground.position.x, ground.position.y, ground.position.z, ground.localScale.x * 10, ground.localScale.z * 10, 0.5f);
-        pathFinder = new AStarPathFinder(pathGrid, this.transform, target, obstacleLayers, ignorePathLayers, false);
+        pathFinder = new AStarPathFinder(pathGrid, this.transform, target, obstacleLayers, ignorePathLayers, true);
         var nodes = pathFinder.pathFinder.pathGrid.nodes;
         List<Node> nodeList = new List<Node>(nodes.GetLength(0) * nodes.GetLength(1));
         for (int i = 0; i < nodes.GetLength(1); i++)
@@ -57,6 +58,7 @@ public class PathAgent : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        if (!UnityEditor.Selection.gameObjects.Contains(transform.gameObject)) { return; }
         if (pathFinder == null || 
             pathFinder.pathFinder.finalPath == null ||
             !enableGizmos) return;
@@ -68,11 +70,26 @@ public class PathAgent : MonoBehaviour
             for (int j = 0; j < nodes.GetLength(0); j++)
             {
                 Node node = nodes[i, j];
-                if (node.isObstacle) Gizmos.color = Color.red;
-                else if (node.dontIncludeToPath) Gizmos.color = Color.yellow;
-                else if (node.ignoreable) Gizmos.color = Color.white;
-                else if (finalPath.Contains(node)) Gizmos.color = Color.green;
-                else Gizmos.color = Color.blue;
+                if (node.isObstacle)
+                {
+                    Gizmos.color = Color.red;
+                }
+                else if (node.dontIncludeToPath)
+                {
+                    Gizmos.color = Color.yellow;
+                }
+                else if (node.ignoreable)
+                {
+                    Gizmos.color = Color.white;
+                }
+                else if (finalPath.Contains(node))
+                {
+                    Gizmos.color = Color.green;
+                }
+                else
+                {
+                    Gizmos.color = Color.blue;
+                }
 
                 Gizmos.DrawCube(node.worldPosition.ToVector3(), Vector3.one);
             }
